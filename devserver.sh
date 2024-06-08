34fe69e46af51852250179b063046be733e7a9d5
Evelio Tarazona <pull@evel.io>
2024-06-08 11:27:17 +0000

    Split setup and devserver

--- devserver.sh
+++ devserver.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 source .venv/bin/activate
-export OLLAMA_MODEL="llama3"
 export CHAINLIT_PORT="${PORT:-8000}"
 export DEBUG=1
 export WATCH=1
-export HF_HUB_ENABLE_HF_TRANSFER=1
 
 printenv
 
-nohup ollama serve &
-sleep 2 && ollama pull $OLLAMA_MODEL
-
-python hf_fetch.py # prefetch datasets
 python main.py
