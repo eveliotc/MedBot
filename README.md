d455657bea0c8ea09de8deee1eec61d98f5ed4d6
dbykovskyy <dbykovskyy@zillowgroup.com>
2024-06-08 02:17:14 -0700

    - added crew ai agent and minor readme and design of landing page

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ A prototype of a chatbot based-off [MedRAG](https://huggingface.co/datasets/MedR
 ```
 OPENAI_API_KEY=sk-...
 ```
+2. Download https://www.ollama.com/ and then `ollama` command line tool
+3. ```ollama run llama3```
+4.  run `chainlit run main.py -w` command from your project root
+
+
+## For big file commit!
+
+### Example:
+-  `brew install git-lfs`
+-  `git lfs install`
+-  `git lfs track "my_index_file_large.faiss"`
+-  `git add my_index_file_large.faiss`
+-  `git commit -m "Add large file`
--- main.py
+++ main.py
@@ -3,8 +3,23 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
-
+from agents.rag_agent import RagAgent
 import chainlit as cl
+
+
+def agent_execute(message):
+    '''
+
+    :param message: user input form Chainlit
+    :return: result after running via CrewAI CoT using specified llm
+    '''
+
+    simple_agent = RagAgent()
+    crew = simple_agent.get_crew()
+    result = crew.kickoff(
+        inputs = {'query': message}
+    )
+    return result
 
 @cl.on_chat_start
 async def on_chat_start():
@@ -50,6 +65,15 @@ async def on_message(message: cl.Message):
         await out_msg.stream_token(chunk)
 
     await out_msg.send()
+
+'''
+#this is Crew AI implementation based on user input. Will merge with rest of the code later
+@cl.on_message
+async def on_message(message: cl.Message):
+    user_input = message.content
+    response = agent_execute(user_input)
+    await cl.Message(response).send()
+'''
 
 if __name__ == "__main__":
     from chainlit.cli import run_chainlit
